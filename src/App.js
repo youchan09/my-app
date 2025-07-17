eae80b232000bf1f89a53f82b987f54da24222fb
youchan09 <noyouchan010@gmail.com>
2025-07-17 10:16:28 +0900

    lastPlease

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🚀 Hello, GitHub Actions!</h1>
+        <p>
+          이 프로젝트는 <strong>CI/CD</strong> 파이프라인을 연습하기 위해 만들어졌어요.
+        </p>
+        <p>
+          <em>이 페이지가 자동으로 배포된다면 성공!</em>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
